b9622f4ad87ed0e363d1aaa0770053946ff5bab3
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-10 13:10:41 +0100

    first router introduced

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,26 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'donate'],
+        name: 'Donate',
+        moduleId: PLATFORM.moduleName('views/donate'),
+        nav: true,
+        title: 'Donate'
+      },
+      {
+        route: 'candidates',
+        name: 'candidates',
+        moduleId: PLATFORM.moduleName('views/candidates'),
+        nav: true,
+        title: 'Candidate'
+      }
+    ]);
+    this.router = router;
+  }
 }
