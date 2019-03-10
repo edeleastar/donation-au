49a89e97604f5389c590fb4b531c9ef5b8a0407a
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-10 11:34:30 +0100

    include candidates view in app

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,2 @@
-import {Candidate} from "./services/donation-types";
-
 export class App {
-  firstName: string;
-  lastName: string;
-  office: string;
-  candidates: Candidate[] = [];
-
-  addCandidate() {
-    const candidate = {
-      firstName: this.firstName,
-      lastName: this.lastName,
-      office: this.office
-    };
-    this.candidates.push(candidate);
-    console.log(candidate);
-  }
 }
