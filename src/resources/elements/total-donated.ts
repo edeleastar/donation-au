26e846eb174d2f66497e6c642ecda6e68e296efa
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-15 12:16:47 +0000

    remove bind to total

--- src/resources/elements/total-donated.ts
+++ src/resources/elements/total-donated.ts
@@ -6,10 +6,10 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 
 @inject(DonationService, EventAggregator)
 export class TotalDonated {
-  @bindable
   total = 0;
 
   constructor(private ds: DonationService, private ea: EventAggregator) {
+    this.total = ds.total;
     ea.subscribe(TotalUpdate, msg => {
       this.total = msg.total;
     });
