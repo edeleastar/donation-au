aecb1799b1012461283adcb379350154c8e4cf32
Eamonn de Leastar <edeleastar@wit.ie>
2019-04-26 19:08:17 +0100

    subscribe to and display donations

--- src/resources/elements/donation-map.ts
+++ src/resources/elements/donation-map.ts
@@ -1,13 +1,31 @@
 import { inject } from 'aurelia-framework';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { LeafletMap } from '../../services/leaflet-map';
+import { TotalUpdate } from '../../services/messages';
+import { Donation } from '../../services/donation-types';
 
+@inject(EventAggregator)
 export class DonationMap {
   mapId = 'donations-map';
   mapHeight = 300;
   map: LeafletMap;
 
-  constructor(private ea: EventAggregator) {}
+  constructor(private ea: EventAggregator) {
+    ea.subscribe(TotalUpdate, msg => {
+      this.renderDonation(msg.donation);
+    });
+  }
+
+  renderDonation(donation: Donation) {
+    if (this.map) {
+      const donationStr = `${donation.candidate.firstName} ${
+        donation.candidate.lastName
+      } £${donation.amount.toString()}`;
+      this.map.addMarker(donation.location);
+      this.map.addMarker(donation.location);
+      this.map.moveTo(12, donation.location);
+    }
+  }
 
   attached() {
     const mapConfig = {
